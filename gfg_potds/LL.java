3b62d1c3ef5183533a515b3c3d25bc388890044a
sumanth-create1 <sajjasatyasumanth@gmail.com>
2026-07-28 21:02:58 +0530

    LinkedList implementation

---
+++ gfg_potds/LL.java
@@ -0,0 +1,110 @@
+package gfg_potds;
+
+public class LL {
+    Node head;
+    private int size;
+
+    LL(){
+        this.size = 0;
+    }
+
+    class Node {
+        String data;
+        Node next;
+
+        Node(String data) {
+            this.data = data;
+            this.next = null;
+            size++;
+        }
+    }
+
+    // add first
+    public void addFirst(String data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    // add Last
+    public void addLast(String data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node currNode = head;
+        while (currNode.next != null) {
+            currNode = currNode.next;
+        }
+        currNode.next = newNode;
+    }
+
+    public void printList() {
+        if (head == null) {
+            System.out.println("The List is Empty");
+            return;
+        }
+        Node currNode = head;
+        while (currNode != null) {
+            System.out.print(currNode.data + "->");
+            currNode = currNode.next;
+        }
+        System.out.println("NULL");
+    }
+
+    public void delteteFirst() {
+        if (head == null) {
+            System.out.println("the list is empty");
+            return;
+        }
+        size--;
+        head = head.next;
+    }
+
+    public void deleteLast() {
+        if (head == null) {
+            System.out.println("the list is empty");
+            return;
+        }
+        size--;
+
+        if(head.next == null) {
+            head = null;
+            return;
+        }
+        Node secondLast = head;
+        Node lastNode = head.next;
+
+        while(lastNode.next != null) {
+            lastNode = lastNode.next;
+            secondLast = secondLast.next;
+        }
+        secondLast.next = null;
+    }
+
+    public int getSize(){
+        return size;
+    }
+
+    public static void main(String[] args) {
+        LL list = new LL();
+        list.addFirst("1");
+        list.addLast("2");
+        list.addLast("3");
+        list.printList();
+
+        list.delteteFirst();
+        list.printList();
+
+        list.deleteLast();
+        list.printList();
+
+        System.out.println(list.getSize());
+
+    }
+}
